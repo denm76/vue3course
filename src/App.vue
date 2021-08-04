13b3a86eb12a3163d2f9c8cb3eebb8687ca1af39
denm76 <denm76@gmail.com>
2021-08-04 15:36:15 +0300

    Применен простейший функционал, созданы модели (data) и функции для обработки моделей(methods), добавлены слушатели событий(@) в templates

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-    
+    <button @click="addLike">Like</button>&nbsp
+    <button @click="addDizLike">Dizlike</button>
+    <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
+    <div>Кол-во дизлайков: <strong>{{ dizlikes }}</strong></div>
 </template>
 
 <script>
-    export default {
-        name: "App"
+
+export default {
+    data(){
+        return {
+            likes:0,
+            dizlikes:0,
+        }
+    },
+    methods: {
+        addLike(){
+            this.likes +=1;
+        },
+        addDizLike(){
+            this.dizlikes +=1;
+        }
     }
+}
+
 </script>
 
 <style scoped>
