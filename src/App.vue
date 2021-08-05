07a309333b227de84f03a4df686d6417483700cf
denm76 <denm76@gmail.com>
2021-08-05 13:52:43 +0300

    Создані компоненты PostList, PostForm(декомпозиция главного компонента App), реализовано двустороннее связывание формы с моделями (инпуты) посредством директивы @input

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,61 @@
 <template>
-    <button @click="addLike">Like</button>&nbsp
-    <button @click="addDizLike">Dizlike</button>
-    <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
-    <div>Кол-во дизлайков: <strong>{{ dizlikes }}</strong></div>
+    <div class="app">
+        <post-form/>
+        <post-list :posts="posts"/>
+
+    </div>
 </template>
 
 <script>
-
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
 export default {
-    data(){
+    components:{
+        PostList, PostForm
+    },
+
+    data() {
         return {
-            likes:0,
-            dizlikes:0,
+            posts: [
+                {id: 1, title: 'Javascript', body: 'Описание поста'},
+                {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+                {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
+            ],
+            title: '',
+            body: '',
         }
     },
+
     methods: {
-        addLike(){
-            this.likes +=1;
+        createPost() {
+            const newPost = {
+                id:Date.now(),
+                title:this.title,
+                body:this.body,
+            }
+            this.posts.push(newPost);
+            this.title = '';
+            this.body = '';
         },
-        addDizLike(){
-            this.dizlikes +=1;
-        }
+        // inputTitle(event){
+        //     this.title = event.target.value;
+        // }
     }
+
 }
 
 </script>
 
-<style scoped>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .app{
+        padding: 20px;
+    }
+
 
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="post" v-for="post in posts">
+        <div><strong>Название:</strong>{{ post.title }}</div>
+        <div><strong>Описание:</strong>{{ post.body }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            posts: {
+                type:Array,//что передаётся
+                required:true //обязательный параметр
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .post{
+        width:100%;
+        padding: 15px;
+        border: 2px solid teal;
+        margin-top: 15px;
+    }
+</style>
