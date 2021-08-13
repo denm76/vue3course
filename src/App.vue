7673da29d7122cd38ff6cd42493f2ded05f0f3da
denm76 <denm76@gmail.com>
2021-08-13 12:47:59 +0300

    Добавлена функци получения постов с сервера по нажатию на кнопку при помощи axios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="app">
         <h1>Страница с постами</h1>
-        <input type="text" v-model.trim="modificatorValue"><!-- trim обрезает пробелы с начала и конца вводимой строки, number приводит к числу-->
+<!--        <input type="text" v-model.trim="modificatorValue">&lt;!&ndash; trim обрезает пробелы с начала и конца вводимой строки, number приводит к числу&ndash;&gt;-->
+        <my-button @click="fetchPosts" style="margin-right: 10px">Получить посты</my-button>
         <my-button
                 @click="showDialog" style="margin:10px 10px">Создать пост</my-button>
         <my-dialog v-model:show = "dialogVisible">
@@ -22,6 +23,8 @@
 <script>
 import PostForm from "@/components/PostForm";
 import PostList from "@/components/PostList";
+import axios from "axios";
+
 export default {
     components:{
         PostList, PostForm
@@ -29,11 +32,7 @@ export default {
 
     data() {
         return {
-            posts: [
-                {id: 1, title: 'Javascript', body: 'Описание поста'},
-                {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
-                {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
-            ],
+            posts: [],
             dialogVisible: false,
             modificatorValue: '',
         }
@@ -42,13 +41,23 @@ export default {
     methods: {
         createPost(post) {
            this.posts.push(post);
-           this.dialogVisible = false
+           this.dialogVisible = false;
         },
         removePost(post){
-            this.posts = this.posts.filter(p => p.id !== post.id)
+            this.posts = this.posts.filter(p => p.id !== post.id);
         },
         showDialog(){
-            this.dialogVisible = true
+            this.dialogVisible = true;
+        },
+        async fetchPosts(){
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+                console.log(response);
+                this.posts = response.data;
+            }
+            catch (e) {
+                alert('Ошибка!');
+            }
         }
         // inputTitle(event){
         //     this.title = event.target.value;
